c30f28da3d77abb13b4f14f093fedd94d37a613a
devjanaprime <dev@primeacademy.io>
2016-08-12 13:01:13 -0500

    added objects for people on call

--- scripts/script.js
+++ scripts/script.js
@@ -6,15 +6,32 @@ function firstFunction(){
 
 var secondFunction = function(greeting){
   console.log( 'in the other function' );
-  var peopleOnThisCall = [ 'Brent', 'Cody', 'Debbie', 'Ian', 'Jazzy', 'Johanna', 'Lisa', 'Luis', 'Ross', 'Dev', 'Millie' ];
-  console.log( 'people on this call:', peopleOnThisCall );
-  /// how many people are in the call today?
+  var peopleOnThisCall = [];
+
+  var newPerson = {
+    name: 'Millie',
+    homeTown: 'Shoreview, suburb of stp'
+  }
+
+  peopleOnThisCall.push(newPerson);
+
+  newPerson = {
+    name: 'Dev',
+    homeTown: 'Neptune'
+  }
+
+  peopleOnThisCall.push(newPerson);
+
+  console.log('People on the call array:', peopleOnThisCall);
+
+  // console.log( 'people on this call:', peopleOnThisCall );
+  // / how many people are in the call today?
   // use ARRAY.length
   // I think Cody got this before I did...
   console.log( 'num of people on call:', peopleOnThisCall.length );
 
   for (var i = 0; i < peopleOnThisCall.length; i++) {
-    console.log(greeting + ", " + peopleOnThisCall[i]);
+    console.log(greeting + ", " + peopleOnThisCall[i].name, "who is from", peopleOnThisCall[i].homeTown );
   }
 };
 
